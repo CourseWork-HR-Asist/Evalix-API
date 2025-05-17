faa73b27fdd67c79c898abf3e5b002b2c59cd0f5
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2025-05-17 20:07:45 +0300

    feat: add requests to llm

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication();
+builder.Services.AddApplication(builder.Configuration);
 
 
 builder.Services.AddOpenApi();

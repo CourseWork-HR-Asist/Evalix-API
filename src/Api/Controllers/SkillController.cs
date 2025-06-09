a1d251bf51809dba81779c5b8130658678f6c2bc
Roman-Davidyuk <roman.davydiuk@oa.edu.ua>
2025-06-09 15:19:22 +0300

    fix: remove authorization check

--- src/Api/Controllers/SkillController.cs
+++ src/Api/Controllers/SkillController.cs
@@ -30,7 +30,7 @@ public class SkillController(ISender sender, ISkillQueries skillQueries) : Contr
 
         return entity.Match<ActionResult<SkillDto>>(r => SkillDto.FromDomainModel(r), () => NotFound());
     }
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     [HttpPost("[action]")]
     public async Task<ActionResult<SkillDto>> Create([FromBody] SkillCreateDto request,
         CancellationToken cancellationToken)
@@ -44,7 +44,7 @@ public class SkillController(ISender sender, ISkillQueries skillQueries) : Contr
 
         return result.Match<ActionResult<SkillDto>>(r => SkillDto.FromDomainModel(r), e => e.ToObjectResult());
     }
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     [HttpPut("[action]/{skillId:guid}")]
     public async Task<ActionResult<SkillDto>> Update([FromRoute] Guid skillId, [FromBody] SkillUpdateDto request, CancellationToken cancellationToken)
     {
@@ -58,7 +58,7 @@ public class SkillController(ISender sender, ISkillQueries skillQueries) : Contr
 
         return result.Match<ActionResult<SkillDto>>(r => SkillDto.FromDomainModel(r), e => e.ToObjectResult());
     }
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     [HttpDelete("[action]/{skillId:guid}")] 
     public async Task<ActionResult<SkillDto>> Delete([FromRoute] Guid skillId, CancellationToken cancellationToken)
     {
